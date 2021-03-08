0a625be63ceb3eecfd22504b4477ffa8cc7defe8
sanjna <sanjna@DESKTOP-7P2VVB4>
2021-03-08 14:18:38 +0530

    First Commit

---
+++ StsProject/src/main/java/sanjana/gupta/in/pro/StsProjectApplication.java
@@ -0,0 +1,13 @@
+package sanjana.gupta.in.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsProjectApplication.class, args);
+	}
+
+}
---
+++ StsProject/src/test/java/sanjana/gupta/in/pro/StsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sanjana.gupta.in.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
