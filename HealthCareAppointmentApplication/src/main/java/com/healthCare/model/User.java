60482517ae8d8a0aa78205f500e9b9395e8a59d7
DELL <DELL@DESKTOP-KAE0S1D>
2021-03-08 21:22:04 +0530

    getter and setter created for User

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/model/User.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/model/User.java
@@ -1,5 +1,62 @@
 package com.healthCare.model;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	public int id;
+	public String username;
+	public String password;;
+	public String role;
+	
+	public User(int id, String username,String password, String role){
+		this.id=id;
+		this.password=password;
+		this.username=username;
+		this.role=role;	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 }
