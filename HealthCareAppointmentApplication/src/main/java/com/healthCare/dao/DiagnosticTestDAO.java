6df642d52d4272c603c9fdce5bdb520a22a19db9
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 10:20:50 +0530

    diagnostictest

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.healthCare.model.DiagnosticTest;
 
 @Repository
-public class DiagnosticTestDAO {
+public class DiagnosticTestDAO  {
 	@Autowired
 	private IDiagnosticTestRepository digTestDao;
 
