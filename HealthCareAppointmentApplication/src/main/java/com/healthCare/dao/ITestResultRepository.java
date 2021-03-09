31bfa20b7bbc505860c876af4ff7b20cd368e294
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 13:16:53 +0530

    Test Result Added + Exception

---
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/ITestResultRepository.java
@@ -0,0 +1,39 @@
+package com.healthCare.dao;
+
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.healthCare.exception.TestResultNotFoundException;
+import com.healthCare.model.Patient;
+import com.healthCare.model.TestResult;
+
+
+@Repository
+public class ITestResultRepository {
+	@Autowired
+	public TestResultDAO testResultDao;
+
+	public TestResult addTestResult(TestResult tr) {
+		TestResult t = testResultDao.save(tr);
+		return t;
+	}
+
+	public TestResult updateTestResult(TestResult tr) {
+		return null;
+	}
+
+	public TestResult removeTestResult(int id) {
+		Optional<TestResult> op=testResultDao.findById(id);
+		
+		TestResult tr= (TestResult) op.orElseThrow(()-> new TestResultNotFoundException("Test Result Does Not Exists"));
+		testResultDao.delete(tr);
+		return tr;
+	}
+	public Set<TestResult> viewResultsByPatient(Patient patient){
+		Set<TestResult> set=testResultDao.findAllByPatient(patient);
+		return set;
+	}
+}
