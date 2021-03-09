3488e0cc0b281454975531e106b2525ec59e76c7
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 08:48:45 +0530

    push

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.healthCare.model.DiagnosticTest;
 
 public interface DiagnosticTestDAO extends JpaRepository<DiagnosticTest, Integer> {
+<<<<<<< Upstream, based on branch 'master' of https://github.com/nomaanhusain/Sprint-Group-6.git
 
 }
+=======
+	
+}
+>>>>>>> ae7e5f5 push
