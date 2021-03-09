b7cecc33d788864e066468e966f5c2ceff1d0b57
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 14:00:09 +0530

    TestResult Service

---
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/service/ITestResultService.java
@@ -0,0 +1,16 @@
+package com.healthCare.service;
+
+import java.util.Set;
+
+import com.healthCare.model.Patient;
+import com.healthCare.model.TestResult;
+
+public interface ITestResultService {
+	public TestResult addTestResult(TestResult tr);
+
+	public TestResult updateTestResult(TestResult tr);
+
+	public TestResult removeTestResult(int id);
+
+	public Set<TestResult> viewResultsByPatient(Patient patient);
+}
---
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/service/ITestResultServiceImpl.java
@@ -0,0 +1,42 @@
+package com.healthCare.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.healthCare.dao.ITestResultRepository;
+import com.healthCare.model.Patient;
+import com.healthCare.model.TestResult;
+
+@Service
+public class ITestResultServiceImpl implements ITestResultService {
+	@Autowired
+	private ITestResultRepository testRep;
+
+	@Override
+	public TestResult addTestResult(TestResult tr) {
+		// TODO Auto-generated method stub
+		TestResult t= testRep.addTestResult(tr);
+		return t;
+	}
+
+	@Override
+	public TestResult updateTestResult(TestResult tr) {
+		TestResult t=testRep.updateTestResult(tr);
+		return t;
+	}
+
+	@Override
+	public TestResult removeTestResult(int id) {
+		TestResult tr=testRep.removeTestResult(id);
+		return tr;
+	}
+
+	@Override
+	public Set<TestResult> viewResultsByPatient(Patient patient) {
+		Set<TestResult> set=testRep.viewResultsByPatient(patient);
+		return set;
+	}
+
+}
