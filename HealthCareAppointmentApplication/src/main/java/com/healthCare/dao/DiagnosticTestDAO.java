d6cf0496e33e09bd7b080aacdb9969b290cd6197
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 09:58:06 +0530

    IdigRep Update

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
@@ -3,7 +3,6 @@ package com.healthCare.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.healthCare.model.DiagnosticTest;
--- HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/IDiagnosticTestRepository.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/IDiagnosticTestRepository.java
@@ -12,4 +12,9 @@ import com.healthCare.model.DiagnosticTest;
 public interface IDiagnosticTestRepository extends JpaRepository<DiagnosticTest, Integer>{
 	public List<DiagnosticTest> findByCenterId(int centerId);
 	public DiagnosticTest deleteByCenterId(int centerId);
+	public List<DiagnosticTest> getAllTest();
+	public DiagnosticTest addNewTest(DiagnosticTest test);
+	public List<DiagnosticTest> getTestsOfDiagnosticCenter(int centerId);
+	public DiagnosticTest updateTestDetail(DiagnosticTest test);
+	public DiagnosticTest removeTestFromDiagnosticCenter(int centerId, DiagnosticTest test);
 }
