da504aca8ce10aba1f37aa8095a22670e76bb18a
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-09 09:29:07 +0530

    Repository Update

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/dao/DiagnosticTestDAO.java
@@ -7,27 +7,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.healthCare.model.DiagnosticTest;
+
 @Repository
-public class DiagnosticTestDAO  {
+public class DiagnosticTestDAO {
 	@Autowired
 	private IDiagnosticTestRepository digTestDao;
 
 	public List<DiagnosticTest> getAllTest() {
-		List<DiagnosticTest> ls=digTestDao.findAll();
+		List<DiagnosticTest> ls = digTestDao.findAll();
 		return ls;
 	}
+
 	public DiagnosticTest addNewTest(DiagnosticTest test) {
-		DiagnosticTest dig= digTestDao.save(test);
+		DiagnosticTest dig = digTestDao.save(test);
 		return dig;
 	}
+
 	public List<DiagnosticTest> getTestsOfDiagnosticCenter(int centerId) {
-		
+		List<DiagnosticTest> list = digTestDao.findByCenterId(centerId);
+		return list;
 	}
-	//This might be wrong, please check!
+
+	// This might be wrong, please check!
 	public DiagnosticTest updateTestDetail(DiagnosticTest test) {
-		DiagnosticTest dig=digTestDao.save(test);
+		DiagnosticTest dig = digTestDao.save(test);
 		return dig;
 	}
+
 	public DiagnosticTest removeTestFromDiagnosticCenter(int centerId, DiagnosticTest test) {
 		
 	}
