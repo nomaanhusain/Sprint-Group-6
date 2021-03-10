c2a97bc3502bdfeb6884c6e3c72d5fc059919497
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-10 09:00:16 +0530

    Correction

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/controller/DiagnosticTestController.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/controller/DiagnosticTestController.java
@@ -13,12 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.healthCare.model.DiagnosticTest;
 import com.healthCare.service.IDiagnosticTestService;
 
 @Controller
+@RestController
+@RequestMapping("/hcdtc")
 public class DiagnosticTestController {
 	@Autowired
 	private IDiagnosticTestService digServices;
