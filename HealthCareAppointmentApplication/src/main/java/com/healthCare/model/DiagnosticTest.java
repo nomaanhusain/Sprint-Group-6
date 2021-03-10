062b9f56dc0a0982d0c3a755313b91c10baf396c
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-10 10:23:35 +0530

    ManytoMany

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/model/DiagnosticTest.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/model/DiagnosticTest.java
@@ -2,9 +2,12 @@ package com.healthCare.model;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -13,10 +16,12 @@ import lombok.Data;
 public class DiagnosticTest {
 	@Id
 	@GeneratedValue
-	private int id;
+	private int testId;
 	private String testName;
 	private double testPrice;
 	private String normalValue;
 	private String units;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="id",referencedColumnName = "testId")
 	private Set<DiagnosticCenter> diagnosticCenter;
 }
