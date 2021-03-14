43553b423b269755d038d832ef68766953e54d96
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-03-14 12:07:12 +0530

    correction

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/model/TestResult.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/model/TestResult.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,10 +20,13 @@ public class TestResult {
 	@Id
 	@GeneratedValue
 	private int testResultId;
+	@ApiModelProperty(notes = "Test Reading")
 	private double testReading;
+	@ApiModelProperty(notes = "Condition")
 	private String condition;
+	@ApiModelProperty(notes = "Appointment Object")
 	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="appointment_id", referencedColumnName = "appointmentId")
+	@JoinColumn(name="appointment_id", referencedColumnName = "appointment_id")
 	private Appointment appointment;
 	
 
