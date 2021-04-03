1af772681cddf177cc5f4c32aadb13193d959a87
Nomaan <Nomaan@DESKTOP-72N6T1P>
2021-04-03 14:19:04 +0530

    Login update

--- HealthCareAppointmentApplication/src/main/java/com/healthCare/controller/LoginController.java
+++ HealthCareAppointmentApplication/src/main/java/com/healthCare/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.healthCare.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,13 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/login")
 @Api(value = "Login", tags = { "Login" }, description = "Controller for Login")
+@CrossOrigin
 public class LoginController {
 	@Autowired
 	private ILoginService loginService;
 	private boolean loginStatus = false;
 
-	@GetMapping(value = "/loginuser")
+	@PostMapping(value = "/loginuser")
 	@ApiOperation(value = "To Login a user")
 	public ResponseEntity<String> validateLogin(@RequestBody Users user) {
 		if (loginStatus) {
@@ -41,11 +43,11 @@ public class LoginController {
 
 	@GetMapping("/logout")
 	@ApiOperation(value = "To Logout a user")
-	public ResponseEntity<String> logout(@RequestBody Users user) {
-		Users u = loginService.logout(user);
+	public ResponseEntity<String> logout() {
+		//Users u = loginService.logout(user);
 		if (loginStatus) {
 			loginStatus = false;
-			return new ResponseEntity<String>("Logged Out User " + u.getUsername(), HttpStatus.OK);
+			return new ResponseEntity<String>("Logged Out User ", HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("Please Login First", HttpStatus.OK);
 	}
